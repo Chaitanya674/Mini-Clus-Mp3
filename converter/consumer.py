201065554b15ff9be19b3943319a00e6e242765a
Chaitanya674 <chaitanyasharma674@gmail.com>
2023-09-11 00:54:42 +0530

    Full Stack Micro Service Project

---
+++ converter/consumer.py
@@ -0,0 +1,44 @@
+import pika , sys, os , time
+from pymongo import MongoClient
+import gridfs
+from convert import to_mp3
+
+def main():
+    client = MongoClient("host.minikube.internal" , 27017)
+    db_videos = client.videos
+    db_mp3s = client.mp3s
+    
+    #gridfs
+    fs_videos = gridfs.GridFS(db_videos)
+    fs_mp3s = gridfs.GridFS(db_mp3s)
+    
+    #rabbitmq connection
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(
+            host="rabbitmq"
+        )
+    )
+    def callback(ch , method, properties , body ):
+        err = to_mp3.start(body, fs_videos , fs_mp3s , ch)
+        if err:
+            ch.basic_nack(delivery_tag=method.delivery_tag)
+        else:
+            ch.basic_ack(delivery_tag= method.delivery_tag)
+        return 
+    channel = connection.channel(
+        queue=os.environ.get("VIDEO_QUEUE"), on_message_callback=callback
+    )
+    
+    print("waiting for the messages. To exit press CRTL + C")
+    
+    channel.start_consumig()
+    
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Interrupted")
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)  
